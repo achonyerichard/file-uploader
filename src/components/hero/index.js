63875ac006f9c4f74706ae6d9605fecd36aa9d2e
Richard <achonyerichard@gmail.com>
2022-06-09 02:37:46 +0100

    2 commit

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -13,9 +13,7 @@ const Hero = () => {
             <h1 className="my-4 text-5xl font-bold leading-tight">
               Welcome to Trex Video Upload Services
             </h1>
-            <p className="leading-normal text-2xl mb-8 font-semibold">
-              Upload and store
-            </p>
+            
             <button className="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
               Subscribe
             </button>
